dfd93baaa9cc4d24a072dae01c51e1b41e32e499
Khaleel <pathankhaleel8@gmail.com>
2023-05-14 22:48:16 +0530

    updating learn-shell

--- vars.sh
+++ vars.sh
@@ -3,3 +3,7 @@ echo "hello, Good Mornning,Today Date is ${DATE}"
 # command substitution
 DATE=$(date)
 echo "hello,Good Morning,Today Date is ${DATE}"
+
+# Arthematic substitutions
+ADD=$((10+20))
+echo "addition of 10+20 =${ADD}"
