cc7b6f0edd00707c5fb855ccb5c5b3dbb84db202
Khaleel <pathankhaleel8@gmail.com>
2023-05-16 18:56:24 +0530

    updating learn-shell

---
+++ functions.sh
@@ -0,0 +1,15 @@
+m1(){
+echo hello world from function
+echo First argument - $1
+echo second argument -- $2
+echo all arguments - $*
+echo no of Arguments - $#
+echo value of a - $a
+b=200
+}
+
+# main function
+## call a function
+a=120
+xyz 123 456
+echo value of b - $b
