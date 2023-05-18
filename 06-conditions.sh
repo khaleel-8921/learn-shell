d529cd366f30a0c07d1c2156deb5c7e50877a401
Khaleel <pathankhaleel8@gmail.com>
2023-05-18 20:27:28 +0530

    updating learn-shell

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -1,11 +1,11 @@
 number=$1
 string=$2
 
-if [ ${number}  -eq 5 ]; then # then can be in same line with semicolon, are with new line
+if [ "${number}" -eq 5 ]; then # then can be in same line with semicolon, are with new line
 echo number is 5
 fi
 
-if [ ${String} == abc ]; then
+if [ "${String}" == abc ]; then
   echo string is abc
 else
   echo string is not abc
