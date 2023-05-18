e972ae6849de903551f8bfc57e516df973f6dc42
Khaleel <pathankhaleel8@gmail.com>
2023-05-18 11:44:41 +0530

    updating learn-shell

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -1,12 +1,17 @@
 number =$1
 string=$2
 
-if [ ${number} -eq 5 ]; then # then can be in same line with semicolon, are with new line
+if [ "${number}"  -eq 5 ]; then # then can be in same line with semicolon, are with new line
 echo number is 5
 fi
-if[ ${String}==abc ]; then
-  echo string is abc;
+
+if[ "${String}" == abc ]; then
+  echo string is abc
+else
+  echo string is not abc
+
 fi
+
 ## it is always good practice to quote he variable in expression
 
 
